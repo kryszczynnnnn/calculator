b88ed12e078303b1598388102058d62e71dc2524
kryszczynnnnn <146872833+kryszczynnnnn@users.noreply.github.com>
2024-03-11 21:22:57 +0100

    Add files via upload

---
+++ source code/script.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("display");
+
+function appendDisplay(input) {
+    display.value = display.value + input
+}
+
+function clearDisplay() {
+
+}
+
+function calculate() {
+
+}
